44db21d527c5426381c52d04540306ab5ff8be6d
Mitsuo Shiota <mitsuo.shiota.xv@hitachi.com>
2020-03-19 11:29:57 +0900

    format python code

--- download.py
+++ download.py
@@ -1,28 +1,38 @@
-import requests, time
+import requests
+import time
 
 for year in range(1947, 2021):
     if year <= 1949:
         # Although there are midyear reports from 1949 to 1952, I ignore them
-        url = 'https://fraser.stlouisfed.org/files/docs/publications/ERP/{0}/ERP_{0}_January.pdf'.format(year)
+        url = 'https://fraser.stlouisfed.org/files/docs/publications/ERP/{0}/ERP_{0}_January.pdf'.format(
+            year)
     elif year <= 1952:
-        url = 'https://fraser.stlouisfed.org/files/docs/publications/ERP/{0}/ERP_January_{0}.pdf'.format(year)
+        url = 'https://fraser.stlouisfed.org/files/docs/publications/ERP/{0}/ERP_January_{0}.pdf'.format(
+            year)
     elif year <= 1986:
-        url = 'https://fraser.stlouisfed.org/files/docs/publications/ERP/{0}/ERP_{0}.pdf'.format(year)
+        url = 'https://fraser.stlouisfed.org/files/docs/publications/ERP/{0}/ERP_{0}.pdf'.format(
+            year)
     elif year <= 1988:
-        url = 'https://fraser.stlouisfed.org/files/docs/publications/ERP/{0}/ER_{0}.pdf'.format(year)
+        url = 'https://fraser.stlouisfed.org/files/docs/publications/ERP/{0}/ER_{0}.pdf'.format(
+            year)
     elif year <= 2008:
-        url = 'https://fraser.stlouisfed.org/files/docs/publications/ERP/{0}/ERP_{0}.pdf'.format(year)
+        url = 'https://fraser.stlouisfed.org/files/docs/publications/ERP/{0}/ERP_{0}.pdf'.format(
+            year)
     elif year == 2009:
-        url = 'https://fraser.stlouisfed.org/files/docs/publications/ERP/{0}/{0}_ERP.pdf'.format(year)
+        url = 'https://fraser.stlouisfed.org/files/docs/publications/ERP/{0}/{0}_ERP.pdf'.format(
+            year)
     elif year == 2010:
-        url = 'https://fraser.stlouisfed.org/files/docs/publications/ERP/{0}/erp_{0}.pdf'.format(year)
+        url = 'https://fraser.stlouisfed.org/files/docs/publications/ERP/{0}/erp_{0}.pdf'.format(
+            year)
     elif year <= 2014:
-        url = 'https://fraser.stlouisfed.org/files/docs/publications/ERP/{0}/{0}_erp.pdf'.format(year)
+        url = 'https://fraser.stlouisfed.org/files/docs/publications/ERP/{0}/{0}_erp.pdf'.format(
+            year)
     elif year <= 2019:
-        url = 'https://fraser.stlouisfed.org/files/docs/publications/ERP/{}_erp.pdf'.format(year)
+        url = 'https://fraser.stlouisfed.org/files/docs/publications/ERP/{}_erp.pdf'.format(
+            year)
     else:
         url = 'https://www.whitehouse.gov/wp-content/uploads/2020/02/2020-Economic-Report-of-the-President-WHCEA.pdf'
-    
+
     output_filename = 'data/before_split/{}_erp.pdf'.format(year)
 
     myfile = requests.get(url)
@@ -32,5 +42,3 @@ for year in range(1947, 2021):
 
     print(year)
     time.sleep(20)
-    
-
